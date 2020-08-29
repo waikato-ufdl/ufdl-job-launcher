45f29a12b1eb6b3303ddfcfc365d18001ea0330f
Peter Reutemann <fracpete@gmail.com>
2020-08-29 18:06:26 +1200

    no longer jobs that have already been acquired

--- src/ufdl/joblauncher/poll/_core.py
+++ src/ufdl/joblauncher/poll/_core.py
@@ -27,11 +27,13 @@ def generate_filter(hardware_info, debug=False):
                                 Compare(field="docker_image.cuda_version.version", operator="<=", value=hardware_info["cuda"]),
                                 Compare(field="docker_image.cuda_version.min_driver_version", operator="<=", value=hardware_info["driver"]),
                                 Compare(field="docker_image.min_hardware_generation.min_compute_capability", operator="<=", value=hardware_info["gpus"][0]["compute"]),
+                                IsNull(field="node", invert=True),
                         ]),
                         And(
                             sub_expressions=[
                                 IsNull(field="start_time"),
-                                Exact(field="docker_image.cpu", value=True)
+                                Exact(field="docker_image.cpu", value=True),
+                                IsNull(field="node", invert=True),
                         ])
                     ])
             ])
@@ -41,7 +43,8 @@ def generate_filter(hardware_info, debug=False):
                 And(
                     sub_expressions=[
                         IsNull(field="start_time"),
-                        Exact(field="docker_image.cpu", value=True)
+                        Exact(field="docker_image.cpu", value=True),
+                        IsNull(field="node", invert=True),
                 ])
             ],
         )
